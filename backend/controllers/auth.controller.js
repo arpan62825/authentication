e188542548878e130e6a51246ca1be28ffa7f0fd
Arpan Das <arpan62825@gmail.com>
2026-06-15 10:45:09 +0530

    chore: verify the token sent via email, and verify the account

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -61,3 +61,33 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   res.send("logout route should work in postman");
 };
+
+export const verifyEmail = async (req, res) => {
+  const { code } = req.body;
+  try {
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Invalid or expired verification token" });
+    }
+
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationTokenExpiresAt = undefined;
+    await user.save();
+
+    // todo => implement welcome email
+    // await sendWelcomeEmail(user.name, user.email)
+
+    return res.status(200).json({ message: "Account has been verified" });
+  } catch (error) {
+    console.log(
+      `An error occurred while trying to verify the account: ${error}`,
+    );
+  }
+};
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { signup, login, logout, verifyEmail } from "../controllers/auth.controller.js";
 
 const route = Router();
 
@@ -8,5 +8,7 @@ route.post("/signup", signup);
 route.post("/login", login);
 
 route.post("/logout", logout);
+
+route.post("/verify-email", verifyEmail);
 
 export default route;
