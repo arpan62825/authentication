920005c07b20bda6e5b129bc1546fecf44d44b15
Arpan Das <arpan62825@gmail.com>
2026-06-18 13:23:56 +0530

    chore: add frontend boilerplate and signup-page initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router";
+import SignupPage from "./pages/SignupPage.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/SignupPage.jsx
@@ -0,0 +1,86 @@
+import { Check } from "lucide-react";
+import {
+  Button,
+  Description,
+  FieldError,
+  Input,
+  Label,
+  Form,
+  TextField,
+} from "@heroui/react";
+
+const SignupPage = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = {};
+
+    // Convert FormData to plain object
+    formData.forEach((value, key) => {
+      data[key] = value.toString();
+    });
+
+    alert(`Form submitted with: ${JSON.stringify(data, null, 2)}`);
+  };
+  return (
+    <div className="flex justify-center items-center h-full">
+      <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+
+            return null;
+          }}
+        >
+          <Label className="text-white">Email</Label>
+          <Input placeholder="john@example.com" />
+          <FieldError />
+        </TextField>
+
+        <TextField
+          isRequired
+          minLength={8}
+          name="password"
+          type="password"
+          validate={(value) => {
+            if (value.length < 8) {
+              return "Password must be at least 8 characters";
+            }
+            if (!/[A-Z]/.test(value)) {
+              return "Password must contain at least one uppercase letter";
+            }
+            if (!/[0-9]/.test(value)) {
+              return "Password must contain at least one number";
+            }
+
+            return null;
+          }}
+        >
+          <Label className="text-white">Password</Label>
+          <Input placeholder="Enter your password" />
+          <Description>
+            Must be at least 8 characters with 1 uppercase and 1 number
+          </Description>
+          <FieldError />
+        </TextField>
+
+        <div className="flex gap-2">
+          <Button type="submit">
+            <Check />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default SignupPage;
